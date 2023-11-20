977e98563be5902080558878a589cc425fc7516d
Graham Marshall <gmarshalltech1@gmail.com>
2023-11-20 10:41:02 -0800

    Added models

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -7,6 +7,27 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+
+    id: {
+      type: DataTypes.INTEGER, // integer
+      allowNull: false, // doesn't allow null values
+      primaryKey: true, // set as primary key
+      autoIncrement: true // uses auto increment
+    },
+    product_id: {
+      type: DataTypes.INTEGER, // integer
+      references: {
+        model: 'product', // references the product model
+        key: 'id' // references the id column in product model
+      }
+    },
+    tag_id: {
+      type: DataTypes.INTEGER, // integer
+      references: {
+        model: 'tag', // references the tag model
+        key: 'id' // references the id column in tag model
+      }
+    },
   },
   {
     sequelize,
